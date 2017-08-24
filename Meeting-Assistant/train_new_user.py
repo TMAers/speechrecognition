4b421088e97a7d5999286b495ab4e496a1655e05
Phu Phan <pthanhphu95@gmail.com>
2017-08-24 15:37:51 +0700

    Update project

--- Meeting-Assistant/train_new_user.py
+++ Meeting-Assistant/train_new_user.py
@@ -19,7 +19,7 @@ n_hidden_units_two = 350
 sd = 1 / np.sqrt(n_dim)
 learning_rate = 0.000008
 batch_size = 290
-n_classes = 4
+n_classes = 3
 
 
 def train_model_w_new_data():
@@ -46,10 +46,10 @@ def train_model_w_new_data():
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
     cost_function = tf.reduce_mean(-tf.reduce_sum(Y * tf.log(y_)))
     optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost_function)
-    features, labels = loadfile.get_audio_features("./", "record218train106")
+    features, labels = loadfile.get_audio_features("./", "datatrain")
     labels = loadfile.one_hot_encode_with_test_file(labels, n_classes)
-    features_test, labels_test = loadfile.get_audio_features("./", "c")
-    labels_test = loadfile.one_hot_encode_with_test_file(labels_test, n_classes)
+    #features_test, labels_test = loadfile.get_audio_features("./", "c")
+    #labels_test = loadfile.one_hot_encode_with_test_file(labels_test, n_classes)
 
     print labels
     """if(num>1):
@@ -70,65 +70,5 @@ def train_model_w_new_data():
     save_path = tf.train.Saver().save(sess, "./model/data")
     print("Model saved")
 
-
-"""
-def get_ten_input(number):
-	sentences=open("TEXT.txt","r")
-        list_ =sentences.readlines()
-	print "Please speak some sentenses below:"
-	for i in range(1,11):
-		print i,". ", list_[randint(0,103)]
-		print "Enter to start record!"
-		p = pyaudio.PyAudio()
-		frames = []
-		enter = raw_input()
-		if enter=="":
-			print "recording !!!"
-			stream = p.open(format=FORMAT,
-                	channels=CHANNELS,
-                	rate=RATE,
-                	input=True,
-                	frames_per_buffer=CHUNK)
-			#enter1=raw_input()
-			for n in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
-    				data = stream.read(CHUNK)
-    				frames.append(data)
-
-			print("done recording !!!")
-
-			stream.stop_stream()
-			stream.close()
-			p.terminate()
-			file_name="./audiodata/"+str(number)+"_"+str(i)+".wav"
-			wf = wave.open(file_name, 'wb')
-			wf.setnchannels(CHANNELS)
-			wf.setsampwidth(p.get_sample_size(FORMAT))
-			wf.setframerate(RATE)
-			wf.writeframes(b''.join(frames))
-			wf.close()
-			print "Saved to ",file_name"""
-
-
-def add_new_user(new_user, users):
-    check = False
-    num_users = 0
-    for user in users:
-        num_users = num_users + 1
-        user = user.split("\n")[0]
-        if new_user == user:
-            print "Your voice data is exist in database"
-            return 0
-    if check == False:
-        users.write(new_user)
-        users.write('\n')
-        print "Added your name to USER.txt"
-    # get_ten_input(num_users+1)
-    return num_users + 1
-
-
 print '************** Train *************'
-"""new_user = raw_input("Enter your name: ")
-users=open("USER.txt","r+")
-num=add_new_user(new_user,users)"""
-# if(num!=0):
 train_model_w_new_data()
